2bf78ffdaf4cc9aafc649b5615882db6c4e7bef4
hakt0-r <VikantShah@yahoo.com>
2016-02-17 08:23:45 +1000

    Update NSreader.User.js
    Version update

--- NSreader.User.js
+++ NSreader.User.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Newscientist past issues infinite scroll + HTML5 e-magazine reader.
 // @namespace    http://tampermonkey.net/hakt0-r/
-// @version      0.1
+// @version      0.3
 // @description  Adds infinite scrolling to Newscientist past issues page
 // @author       hakt0r, CapType
 // @match        https://www.newscientist.com/issues/
